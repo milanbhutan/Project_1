048c0a077bcfbc78377e547a378b44372469110c
milanbhutan <milanbhutan@gmail.com>
2026-09-12 10:14:31 -0700

    Computed temperature and precipitation variances and the covariance.

--- Part_2.py
+++ Part_2.py
@@ -1,18 +1,34 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import pandas as pd
-import my_stats_lib as my_stats
+import my_stats_lib as stats
 
-df_temp_prcp = pd.read_csv("Datasets/SLO_annual_temp_and_precip.csv")
-annual_max_temp = df_temp_prcp.loc[:, "TMAX"]
-annual_prcp = df_temp_prcp.loc[:, "PRCP"]
+df_temp_prcp = pd.read_csv("SAN LUIS OBISPO POLY WEATHER.csv")
+df_temp_prcp_fixed = (
+    df_temp_prcp[["DATE", "TMAX", "PRCP"]]
+    .dropna()
+    .set_index("DATE")
+    .sort_index()
+    )
+annual_max_temp = df_temp_prcp_fixed.loc[:, "TMAX"].to_numpy()
+annual_prcp = df_temp_prcp_fixed.loc[:, "PRCP"].to_numpy()
+
+avg_max_temp = stats.avg(annual_max_temp)
+avg_prcp = stats.avg(annual_prcp)
+
+var_max_temp = stats.var(annual_max_temp)
+var_prcp = stats.var(annual_prcp)
+temp_prcp_covar = stats.covar(annual_max_temp, annual_prcp)
 
 
-avg_max_temp = my_stats.avg(annual_max_temp)
-avg_prcp = my_stats.avg(annual_prcp)
-# my_covar = my_stats.covar(x, y)
+fig, axes = plt.subplots(2, 3)
 
-print(avg_max_temp)
-print(avg_prcp)
+axes[0, 0].plot(df_temp_prcp_fixed[["TMAX"]])
 
-#print(my_covar)
+print(f"Max Temperature Average: {avg_max_temp}")
+print(f"Precipitation Average: {avg_prcp}")
+print(f"Max Temperature Variance: {var_max_temp}")
+print(f"Precipitation Variance: {var_prcp}")
+print(f"Temperature and Precipitation Covariance: \n{temp_prcp_covar}")
+
+plt.show()
--- my_stats_lib.py
+++ my_stats_lib.py
@@ -4,16 +4,22 @@ def avg(
     x: np.ndarray
 ):
     return (np.sum(x) / np.size(x) )
+def var(
+    x: np.ndarray
+) -> float:
+    mu_x = avg(x)
+    variance = np.sum((x - mu_x) ** 2) / np.size(x)
+    return variance
 
 
 def covar(
     x: np.ndarray,
     y: np.ndarray,
 ) -> np.ndarray:
-    u_x = avg(x)
-    u_y = avg(y)
+    mu_x = avg(x)
+    mu_y = avg(y)
     z = np.vstack((x, y))
-    mean_vec = np.array([[u_x], [u_y]])
-    covariance = ((z - mean_vec) @ (z - mean_vec).T) / (np.size(x) - 1)
+    mean_vec = np.array([[mu_x], [mu_y]])
+    covariance = ((z - mean_vec) @ (z - mean_vec).T) / np.size(x)
 
     return covariance
